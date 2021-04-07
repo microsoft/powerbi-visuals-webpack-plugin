8b13f80283eb94a9b3ca46a074b0ab8b22ca1d46
Alexey <39551830+Demonkratiy@users.noreply.github.com>
2021-04-07 14:11:33 +0300

    Code refactoring to remove lodash.template usage (#62)
    * Code refactoring to remove lodash.template usage
    
    * Resolved eslint issues
    
    * eslint fix
    
    * resolved PR comments
    
    * replaced eslint by lint in git build

---
+++ templates/package-json-template.js
@@ -0,0 +1,19 @@
+module.exports = function (templateOptions) {
+	return `{
+    "version": ${JSON.stringify(templateOptions.visualData.version)},
+    "author": ${JSON.stringify(templateOptions.authorData)},
+    "resources": [
+        {
+            "resourceId": "rId0",
+            "sourceType": 5,
+            "file": "resources/${templateOptions.guid}.pbiviz.json"
+        }
+    ],
+    "visual": ${JSON.stringify(templateOptions.visualData)},
+    "metadata": {
+        "pbivizjson": {
+            "resourceId": "rId0"
+        }
+    }  
+}`;
+};
