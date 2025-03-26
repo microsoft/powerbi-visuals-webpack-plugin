3bfd7d9da823d5dffa12f4551f005a06b8f3cc75
Ace Nassri <acenassri@microsoft.com>
2025-03-25 18:41:31 -0700

    Add dynamic {capabilities, dependencies} configs

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,46 @@
-module.exports = {
-	populateErrors: (errors, fileName, type) => {
-		if (!errors || errors.length) return;
+const fs = require('fs');
+const { ENCODING } = require('./constants');
+const { file } = require('jszip');
 
-		return errors.map((e) => ({
-			filename: fileName,
-			message: e.stack || "Unknown error",
-			type: type,
-		}));
-	},
+const safelyImport = async (filePath) => {
+	try {
+		const config = (await import(`file://${filePath}`)).default;
+		return config;
+	} catch (e) {
+		console.error(`Error importing JS config from ${filePath}`, e);
+		return null;
+	}
+};
+
+const safelyParse = (filePath) => {
+	try {
+		return JSON.parse(fs.readFileSync(filePath, ENCODING));
+	}
+	catch (e) {
+		console.error(`Error parsing JSON config from ${filePath}`, e);
+		return null;
+	}
+};
+
+const safelyReadConfig = async (filePath) => {
+	return filePath.endsWith('js') ?
+		(await safelyImport(filePath)) :
+		safelyParse(filePath);
+};
+
+const populateErrors = (errors, fileName, type) => {
+	if (!errors || errors.length) return;
+
+	return errors.map((e) => ({
+		filename: fileName,
+		message: e.stack || "Unknown error",
+		type: type,
+	}));
+};
+
+module.exports = {
+	safelyImport,
+	safelyParse,
+	safelyReadConfig,
+	populateErrors
 };
